32b0f329ce7d46bb1e0dd0fb0a2fc4b12c2e0c1f
sheetalvarsh <varsh23@iastate.edu>
2023-10-01 12:59:31 -0500

    Corrected and added the CSS
    - Moved css to static folder
    - Added css path in HTML
    - Moved JS to static folder

--- project/app.py
+++ project/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify, send_from_directory
 from gtts import gTTS
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
