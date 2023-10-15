ea8440b100b1146aa516b7905e006ed4ddf5ad68
sheetalvarsh <varsh23@iastate.edu>
2023-10-14 23:49:55 -0500

    Resolved issues
    - Added storage of messages
    - Resolved: message not being played after conversion
    - added different functions in app.py for modularity

--- project/app.py
+++ project/app.py
@@ -1,85 +1,58 @@
+
+import os
 from flask import Flask, render_template, request, jsonify, send_from_directory
 from gtts import gTTS
-# from speech_recognition import recognize_speech
-import os
 
-# import pyttsx3
-import soundfile as sf
-# import pygame
+app = Flask(__name__)
 
-audio_messages = []  # Initialize an empty list to store message history
+# Define the directory for audio files
+audio_directory = os.path.join(app.root_path, 'static', 'audio')
+if not os.path.exists(audio_directory):
+    os.makedirs(audio_directory)
 
-app = Flask(__name__, static_url_path='/static')
+audio_messages = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', audio_messages = audio_messages)
+    return render_template('index.html', audio_messages=audio_messages)
 
 @app.route('/text-to-speech', methods=['POST'])
 def text_to_speech():
     try:
         message = request.form.get('message-input')
-       
-        # Create a unique filename for each audio message
-        output_filename = f'output_{len(audio_messages) + 1}.ogg'
-        output_audio_path = os.path.join('project/static', 'audio', output_filename)
-        # output_audio_path = os.path.join('static', 'audio', output_filename)
+        print('msg', message)
+        if message:
+            # Create a unique filename for each audio message
+            output_filename = f'output_{len(audio_messages) + 1}.ogg'
+            output_audio_path = os.path.join(audio_directory, output_filename)
 
-        print('hihihihih', os.getcwd())
+            # Convert the message to speech using gTTS
+            tts = gTTS(message, lang='en')
+            tts.save(output_audio_path)
 
-        # Convert the message to speech using gTTS
-        tts = gTTS(message, lang='en')
-        tts.save(output_audio_path)
+            # Add the new audio message to the audio_messages list
+            audio_messages.append(output_filename)
 
-        # Add the new audio message to the audio_messages list
-        audio_messages.append(output_audio_path)
+            # Limit the list to store only the last 5 audio messages
+            if len(audio_messages) > 5:
+                old_audio_filename = audio_messages.pop(0)
+                old_audio_path = os.path.join(audio_directory, old_audio_filename)
+                os.remove(old_audio_path)  # Remove the oldest message
 
-        # Limit the list to store only the last 5 audio messages
-        if len(audio_messages) > 5:
-            os.remove(audio_messages.pop(0))  # Remove the oldest message
+            return jsonify({'recognized_text': message, 'user_audio': output_filename})
+        else:
+            return jsonify({'error': 'No text to speak'})
 
-        return render_template('index.html', audio_messages=audio_messages)
-    
     except Exception as e:
         return jsonify({'error': str(e)})
-    
+
+@app.route('/get-audio/<filename>')
+def get_audio(filename):
+    return send_from_directory(audio_directory, filename)
+
 @app.route('/get-audio-messages', methods=['GET'])
 def get_audio_messages():
     return jsonify({'audio_messages': audio_messages})
-
-# @app.route('/text-to-speech', methods=['GET'])
-# def text_to_speech():
-#     try:
-#         engine = pyttsx3.init()
-#         engine.setProperty('rate', 150)  # Adjust the rate as needed
-#         message = 'This is a very long test message to test the text to speech functionality.'
-
-#         # Save the text as an audio file
-#         engine.save_to_file(message, 'output.wav')
-#         engine.runAndWait()
-
-#         # Load audio data using soundfile
-#         data, samplerate = sf.read('output.wav')
-
-#         # Adjust the samplerate if needed
-#         samplerate = samplerate // 2
-
-#         # Initialize pygame mixer
-#         pygame.mixer.init(frequency=samplerate)
-
-#         # Convert audio data to bytes
-#         audio_bytes = (data * 32767).astype('int16').tobytes()
-
-#         # Load audio into pygame mixer
-#         sound = pygame.mixer.Sound(audio_bytes)
-
-#         # Play audio using pygame mixer
-#         sound.play()
-
-#         return 'Text-to-speech functionality executed successfully'
-#     except Exception as e:
-#         return f'Error: {str(e)}'
-
 
 # Speech to text 
 @app.route('/speech-to-text', methods=['POST'])
--- project/static/script.js
+++ project/static/script.js
@@ -1,6 +1,15 @@
+
 // function textToSpeech() {
+//   event.preventDefault();
 //   // Get the text input
 //   var messageInput = document.getElementById('message-input').value;
+//   console.error('msg - ', messageInput);
+
+//   // Check if there is text to speak
+//   if (messageInput.trim() === '') {
+//     alert('Please enter a message to speak.');
+//     return;
+//   }
 
 //   // Send a POST request for text-to-speech
 //   fetch('/text-to-speech', {
@@ -15,70 +24,73 @@
 //       // Update the recognized text in the textarea
 //       document.getElementById('output-text').value = data.recognized_text;
 
-//       // Set the audio source to the generated audio
-//       document.querySelector("audio source").src = data.user_audio;
+//       // Get the audio controls container
+//       var audioContainer = document.querySelector(".audio-container ul");
+//       audioContainer.innerHTML = "";
 
-//       // Update the audio player
-//       var audio = document.querySelector("audio");
-//       audio.load();
-//       audio.play();
+//       // Create an audio element for the new audio message
+//       var audioElement = document.createElement("audio");
+//       audioElement.controls = true;
+
+//       // Construct the audio message URL based on the returned data
+//       audioElement.src = '/get-audio/' + data.user_audio;
+//       audioElement.type = "audio/ogg";
+
+//       // Create a list item for the audio message
+//       var listItem = document.createElement("li");
+//       listItem.appendChild(audioElement);
+//       audioContainer.appendChild(listItem);
+
+//       audioElement.load();
+//       // audioElement.play();
 //     })
 //     .catch(error => {
 //       console.error('Error:', error);
 //     });
 // }
-document.addEventListener("DOMContentLoaded", function () {
-  function textToSpeech() {
-    // Get the text input
-    var messageInput = document.getElementById('message-input').value;
 
-    // Send a POST request for text-to-speech
-    fetch('/text-to-speech', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: 'message-input=' + encodeURIComponent(messageInput),
-    })
-      .then(response => response.text())
-      .then(data => {
-        // Update the recognized text in the textarea
-        document.getElementById('output-text').value = data;
+function textToSpeech() {
+  // Get the text input
+  var messageInput = document.getElementById('message-input').value;
 
-        // Get the audio controls container
-        var audioContainer = document.querySelector(".audio-container ul");
-        audioContainer.innerHTML = "";
-
-        // Request the latest audio messages and update the audio player
-        fetch('/get-audio-messages')
-          .then(response => response.json())
-          .then(data => {
-            data.audio_messages.forEach(function (audioMessage) {
-              var audioElement = document.createElement("audio");
-              audioElement.controls = true;
-              var sourceElement = document.createElement("source");
-              // Construct the audio message URL using url_for
-              sourceElement.src = '{{ url_for("static", filename="' + audioMessage + '") }}';
-              sourceElement.type = "audio/ogg";
-              audioElement.appendChild(sourceElement);
-              var listItem = document.createElement("li");
-              listItem.appendChild(audioElement);
-              audioContainer.appendChild(listItem);
-
-              var audio = document.querySelector("audio");
-              audio.load();
-              audio.play();
-            });
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+  // Check if the message is not empty
+  if (messageInput.trim() === '') {
+    alert('Please enter a message before sending.');
+    return;
   }
-});
+
+  // Send a POST request for text-to-speech
+  fetch('/text-to-speech', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: 'message-input=' + encodeURIComponent(messageInput),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Update the recognized text in the textarea
+      document.getElementById('output-text').value = data.recognized_text;
+
+      // Get the audio controls container
+      var audioContainer = document.querySelector(".audio-container ul");
+      audioContainer.innerHTML = "";
+
+      // Add the new audio message to the audio player
+      var audioElement = document.createElement("audio");
+      audioElement.controls = true;
+      var sourceElement = document.createElement("source");
+      sourceElement.src = data.user_audio;
+      sourceElement.type = "audio/ogg";
+      audioElement.appendChild(sourceElement);
+      var listItem = document.createElement("li");
+      listItem.appendChild(audioElement);
+      audioContainer.appendChild(listItem);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
 
 
 function speechToText() {
