dd79a7ac4c97959b557dae12112dab94504a6496
sheetalvarsh <varsh23@iastate.edu>
2023-10-24 08:37:38 -0500

    Added CSV file
    - added csv file using pandas
    - added audio file to gitignore
    - updated index.html

--- project/app.py
+++ project/app.py
@@ -1,5 +1,5 @@
-
 import os
+import csv
 from flask import Flask, render_template, request, jsonify, send_from_directory
 from gtts import gTTS
 
@@ -10,7 +10,38 @@ audio_directory = os.path.join(app.root_path, 'static', 'audio')
 if not os.path.exists(audio_directory):
     os.makedirs(audio_directory)
 
+# Define the path for the audio metadata CSV file
+audio_metadata_file = os.path.join(app.root_path, 'audio_metadata.csv')
+
+# Create an empty list to store audio messages
 audio_messages = []
+
+# Define a function to save audio metadata to the CSV file
+def save_audio_metadata(metadata):
+    if not os.path.exists(audio_metadata_file):
+        with open(audio_metadata_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['MessageID', 'MessageText', 'AudioFileName'])
+
+    with open(audio_metadata_file, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(metadata)
+
+# Define a function to load audio metadata from the CSV file
+def load_audio_metadata():
+    if not os.path.exists(audio_metadata_file):
+        return []
+
+    metadata = []
+    with open(audio_metadata_file, newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            metadata.append(row)
+
+    return metadata
+
+# Load existing audio metadata
+audio_messages = load_audio_metadata()
 
 @app.route('/')
 def index():
@@ -20,7 +51,6 @@ def index():
 def text_to_speech():
     try:
         message = request.form.get('message-input')
-        print('msg', message)
         if message:
             # Create a unique filename for each audio message
             output_filename = f'output_{len(audio_messages) + 1}.ogg'
@@ -31,52 +61,34 @@ def text_to_speech():
             tts.save(output_audio_path)
 
             # Add the new audio message to the audio_messages list
-            audio_messages.append(output_filename)
+            audio_messages.append({
+                'MessageID': len(audio_messages) + 1,
+                'MessageText': message,
+                'AudioFileName': output_filename
+            })
 
-            # Limit the list to store only the last 5 audio messages
-            if len(audio_messages) > 5:
-                old_audio_filename = audio_messages.pop(0)
-                old_audio_path = os.path.join(audio_directory, old_audio_filename)
-                os.remove(old_audio_path)  # Remove the oldest message
+            # Save audio metadata to the CSV file
+            save_audio_metadata([len(audio_messages), message, output_filename])
 
-            return jsonify({'recognized_text': message, 'user_audio': output_filename})
+            # Return metadata of the created audio message
+            return jsonify({
+                'recognized_text': message,
+                'user_audio': output_filename
+            })
         else:
             return jsonify({'error': 'No text to speak'})
 
     except Exception as e:
         return jsonify({'error': str(e)})
 
+@app.route('/load-audio-messages', methods=['GET'])
+def load_audio_messages():
+    audio_messages = load_audio_metadata()
+    return jsonify({'audio_messages': audio_messages})
+
 @app.route('/get-audio/<filename>')
 def get_audio(filename):
     return send_from_directory(audio_directory, filename)
-
-@app.route('/get-audio-messages', methods=['GET'])
-def get_audio_messages():
-    return jsonify({'audio_messages': audio_messages})
-
-# Speech to text 
-@app.route('/speech-to-text', methods=['POST'])
-def speech_to_text():
-    try:
-        audio_file = request.files['audio-input']
-        if not audio_file:
-            return jsonify({'error': 'No audio file provided'})
-
-        input_audio_path = 'project/static/audio/input.wav'
-        audio_file.save(input_audio_path)
-        print(input_audio_path)
-
-        # Perform speech recognition using the separate module
-        recognized_text = recognize_speech(input_audio_path)
-        print('msg-', recognized_text)
-
-        return jsonify({'recognized_text': recognized_text})
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-@app.route('/static/<path:filename>')
-def download_file(filename):
-    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- project/static/script.js
+++ project/static/script.js
@@ -1,15 +1,12 @@
-
 function textToSpeech() {
-  // Get the text input
-  var messageInput = document.getElementById('message-input').value;
+  const messageInput = document.getElementById('message-input').value.trim();
+  const outputText = document.getElementById('output-text');
 
-  // Check if the message is not empty
-  if (messageInput.trim() === '') {
+  if (messageInput === '') {
     alert('Please enter a message before sending.');
     return;
   }
 
-  // Send a POST request for text-to-speech
   fetch('/text-to-speech', {
     method: 'POST',
     headers: {
@@ -19,46 +16,60 @@ function textToSpeech() {
   })
     .then(response => response.json())
     .then(data => {
-      // Update the recognized text in the textarea
-      document.getElementById('output-text').value = data.recognized_text;
-
-      // Get the audio controls container
-      var audioContainer = document.querySelector(".audio-container ul");
-      audioContainer.innerHTML = "";
-
-      // Add the new audio message to the audio player
-      var audioElement = document.createElement("audio");
-      audioElement.controls = true;
-      var sourceElement = document.createElement("source");
-      sourceElement.src = data.user_audio;
-      sourceElement.type = "audio/ogg";
-      audioElement.appendChild(sourceElement);
-      var listItem = document.createElement("li");
-      listItem.appendChild(audioElement);
-      audioContainer.appendChild(listItem);
-
-      //temporary fix, will be removed
-      location.reload();
+      outputText.value = data.recognized_text;
     })
     .catch(error => {
       console.error('Error:', error);
     });
 }
 
-function speechToText() {
-  var audioInput = document.getElementById('audio-input').files[0];
+function loadAudioMessages() {
+  fetch('/load-audio-messages', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      const audioMessages = data.audio_messages;
+      const audioContainer = document.querySelector(".audio-container ul");
+      audioContainer.innerHTML = "";
 
-  var formData = new FormData();
+      audioMessages.forEach(audioMessage => {
+        const listItem = document.createElement("li");
+        const audioElement = document.createElement("audio");
+        audioElement.controls = true;
+
+        const sourceElement = document.createElement("source");
+        sourceElement.src = audioMessage.AudioFileName;
+        sourceElement.type = "audio/ogg";
+
+        listItem.appendChild(audioElement);
+        audioElement.appendChild(sourceElement);
+
+        audioContainer.appendChild(listItem);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+// Call the loadAudioMessages function to load and display audio messages
+loadAudioMessages();
+
+function speechToText() {
+  const audioInput = document.getElementById('audio-input').files[0];
+  const formData = new FormData();
   formData.append('audio-input', audioInput);
 
-  // Send a POST request for speech-to-text
   fetch('/speech-to-text', {
     method: 'POST',
     body: formData,
   })
     .then(response => response.json())
     .then(data => {
-      // Update the recognized text in the textarea
       document.getElementById('output-text').value = data.recognized_text;
     })
     .catch(error => {
