b769ed60d6c58acd2c774b000041e6ac2067d1c9
sheetalvarsh <varsh23@iastate.edu>
2023-10-28 14:18:01 -0500

    removed unwanted files

--- project/speech_recognition.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-
-def recognize_speech(audio_file_path):
-    recognizer = sr.Recognizer()
-    with sr.AudioFile(audio_file_path) as source:
-        audio = recognizer.record(source)
-    recognized_text = recognizer.recognize_google(audio, language='en-US')
-    return recognized_text
